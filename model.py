64f6bb09abbb667770a895fa4a8e48aa9f1ab900
RetepRennelk <peterklenner@gmx.de>
2019-01-04 10:39:11 +0100

    First steps towards loading from an ndt-file.

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 from PyQt5.QtCore import QAbstractItemModel
-
+from nestedtreedictionary import NestedDictionaryTree
 
 class TreeModel(QAbstractItemModel):
     def __init__(self):
         super().__init__()
+        self.ndt = NestedDictionaryTree()
+
+    def loadFile(self, filename):
+        self.ndt = NestedDictionaryTree.createTreeFromFile(filename)
--- treeview.py
+++ treeview.py
@@ -1,6 +1,20 @@
-from PyQt5.QtWidgets import QTreeView
-
+from PyQt5.QtWidgets import QTreeView, QShortcut, QFileDialog
+from PyQt5.QtGui import QKeySequence
 
 class TreeView(QTreeView):
     def __init__(self, model):
         super().__init__()
+        self.setModel(model)
+
+        shortcut = QShortcut(QKeySequence("Ctrl+O"), self)
+        shortcut.activated.connect(self.openFile)
+
+    def openFile(self):
+        fd = QFileDialog(self)
+        fd.setNameFilter("Arborg files (*.ndt)")
+        if fd.exec():
+            filenames = fd.selectedFiles()
+            self.model().loadFile(filenames[0])
+
+
+
